8e9114370c21164a69387c12e5a2d583b76ab856
Ajmat <ajmat1130666@gmail.com>
2024-09-10 14:42:26 +0530

    10th September 2024

--- L_Insert_Greatest_Common_Divisor_in_Linked_List.cpp
+++ L_Insert_Greatest_Common_Divisor_in_Linked_List.cpp
@@ -1,46 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    int gcd(int a, int b)
-    {
-        if(a == b) return a;
-        if(a > b) return gcd(a-b, b);
-        else return gcd(a, b-a);
+    int findGCD(int a, int b) {
+        if(b == 0) return a;
+        return findGCD(b, a % b);
     }
-    
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-//         if(head -> next == NULL) 
-//         {
-//             ListNode* newNode = new ListNode(head -> val);
-//             head -> next = newNode;
-//             return head;
-//         }
-        
-        ListNode* temp1 = head;
-        ListNode* temp2 = head -> next;
-        
-        while(temp2 != NULL)
-        {
-            int common = gcd(temp1 -> val, temp2 -> val);
-            
-            ListNode* newNode = new ListNode(common);
-            temp1 -> next = newNode;
-            newNode -> next = temp2;
-            
-            // shifting of nodes:
-            temp2 = temp2 -> next;
-            temp1 = newNode -> next;
+        ListNode* first = head;
+        ListNode* second = head -> next;
+        while(second != nullptr) {
+            int gcdd = findGCD(first -> val, second -> val);
+            ListNode* newNode = new ListNode(gcdd);
+            first -> next = newNode;
+            newNode -> next = second;
+            first = second;
+            second = second -> next;
         }
-        
         return head;
     }
 };
