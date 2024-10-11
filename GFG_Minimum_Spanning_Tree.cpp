a2f8fda409233f208032b35516cd315cad1af10b
Ajmat <ajmat1130666@gmail.com>
2024-10-11 21:57:57 +0530

    11th October 2024

--- GFG_Minimum_Spanning_Tree.cpp
+++ GFG_Minimum_Spanning_Tree.cpp
@@ -1,68 +1,33 @@
-// TC = O(ElogE) and SC = O(E+V)
-class Solution
-{
+// ElogE (pq iterations and pop operation) + 2ElogE (inner adj list total traversal and push operation) is time and V + E is the space complexity:
+class Solution {
 	public:
-    int spanningTree(int V, vector<vector<int>> adj[]){
-        // second step: make a pq for storing node with min weight {{weight, node}, parent}
-        priority_queue<pair<pair<int, int>, int>, vector<pair<pair<int, int>, int>>, greater<pair<pair<int, int>, int>>> q;
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        int ansSum = 0;
+        vector<int> vis (V, 0);
+        vector<pair<int, int>> mst;
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({0, {0, -1}}); // weight, node, parent
         
-        // third step: creating visited, key and parent vectors:
-        vector<bool> vis (V);
-        vector<int> key (V);
-        vector<int> parent (V);
-        
-        // initializing key, vis and parent vector:
-        for(int i=0; i<V; i++)
-        {
-            parent[i] = -1;
-            key[i] = INT_MAX;
-            vis[i] = false;
-        }
-        
-        key[0] = 0;
-        parent[0] = -1;
-        
-        // put 0 node into the pq:
-        q.push({{0, 0}, -1}); // parent of 0 is -1
-        
-        while(!q.empty())
-        {
-            auto top = q.top();
-            q.pop();
+        while(!pq.empty()) {
+            int w = pq.top().first;
+            int node = pq.top().second.first;
+            int parent = pq.top().second.second;
+            pq.pop();
             
-            int node = top.first.second;
-            int weight = top.first.first;
-            int parentNode = top.second;
-            
-            // checking if node is already visited:
             if(vis[node]) continue;
             
-            // if not visited, mark it visited: here you are adding it to mst
-            vis[node] = true;
+            ansSum += w;
+            vis[node] = 1;
+            if(parent != -1) mst.push_back({node, parent});
             
-            // checking for adjacent node:
-            for(auto i: adj[node])
-            {
+            for(auto i: adj[node]) {
                 int adjNode = i[0];
                 int weight = i[1];
                 
-                if(!vis[adjNode] && weight < key[adjNode])
-                {
-                    q.push({{weight, adjNode}, node});
-                    
-                    parent[adjNode] = node;
-                    key[adjNode] = weight;
-                }
+                if(vis[adjNode] == 0) pq.push({weight, {adjNode, node}});
             }
         }
         
-        // creaing ans:
-        int ans = 0;
-        for(int i=1; i<parent.size(); i++)
-        {
-            ans += key[i];
-        }
-        
-        return ans;
+        return ansSum;
     }
 };
